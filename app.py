49abe5f9ae8d9ae3dc320651af59306dc4230508
ekal8 <75772252+ekal8@users.noreply.github.com>
2024-06-05 22:44:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import numpy as np
 import pickle
 import sklearn
 
-DATASET_PATH = "data/heart_2020_cleaned.csv"
-LOG_MODEL_PATH = "model/logistic_regression.pkl"
+DATASET_PATH = "data/ISP DATA HEART DISEASES.csv"
+LOG_MODEL_PATH = "model/LR_model.pkl"
 
 
 def main():
@@ -30,10 +30,6 @@ def main():
         sleep_time = st.sidebar.number_input("How many hours on average do you sleep?", 0, 24, 7)
         gen_health = st.sidebar.selectbox("How can you define your general health?",
                                           options=(gen_health for gen_health in heart.GenHealth.unique()))
-        phys_health = st.sidebar.number_input("For how many days during the past 30 days was"
-                                              " your physical health not good?", 0, 30, 0)
-        ment_health = st.sidebar.number_input("For how many days during the past 30 days was"
-                                              " your mental health not good?", 0, 30, 0)
         phys_act = st.sidebar.selectbox("Have you played any sports (running, biking, etc.)"
                                         " in the past month?", options=("No", "Yes"))
         smoking = st.sidebar.selectbox("Have you smoked at least 100 cigarettes in"
@@ -41,52 +37,39 @@ def main():
                                        options=("No", "Yes"))
         alcohol_drink = st.sidebar.selectbox("Do you have more than 14 drinks of alcohol (men)"
                                              " or more than 7 (women) in a week?", options=("No", "Yes"))
-        stroke = st.sidebar.selectbox("Did you have a stroke?", options=("No", "Yes"))
-        diff_walk = st.sidebar.selectbox("Do you have serious difficulty walking"
-                                         " or climbing stairs?", options=("No", "Yes"))
-        diabetic = st.sidebar.selectbox("Have you ever had diabetes?",
-                                        options=(diabetic for diabetic in heart.Diabetic.unique()))
         asthma = st.sidebar.selectbox("Do you have asthma?", options=("No", "Yes"))
-        kid_dis = st.sidebar.selectbox("Do you have kidney disease?", options=("No", "Yes"))
-        skin_canc = st.sidebar.selectbox("Do you have skin cancer?", options=("No", "Yes"))
 
         features = pd.DataFrame({
-            "PhysicalHealth": [phys_health],
-            "MentalHealth": [ment_health],
             "SleepTime": [sleep_time],
             "BMICategory": [bmi_cat],
             "Smoking": [smoking],
             "AlcoholDrinking": [alcohol_drink],
-            "Stroke": [stroke],
-            "DiffWalking": [diff_walk],
             "Sex": [sex],
             "AgeCategory": [age_cat],
             "Race": [race],
-            "Diabetic": [diabetic],
             "PhysicalActivity": [phys_act],
             "GenHealth": [gen_health],
             "Asthma": [asthma],
-            "KidneyDisease": [kid_dis],
-            "SkinCancer": [skin_canc]
+
         })
 
         return features
 
 
     st.set_page_config(
-        page_title="Heart Disease Prediction App",
+        page_title="Heart Disease Prediction Web Application",
         page_icon="images/heart-fav.png"
     )
 
     st.title("Heart Disease Prediction")
     st.subheader("Are you wondering about the condition of your heart? "
-                 "This app will help you to diagnose it!")
+                 "This application will help you to diagnose it!")
 
     col1, col2 = st.columns([1, 3])
 
     with col1:
         st.image("images/doctor.png",
-                 caption="I'll help you diagnose your heart health! - Dr. Logistic Regression",
+                 caption="I'll help you diagnose your heart health! - Dr. Machine Learning",
                  width=150)
         submit = st.button("Predict")
     with col2:
@@ -108,11 +91,7 @@ def main():
         This model would never be adopted by health care facilities because of its less
         than perfect accuracy, so if you have any problems, consult a human doctor.**
         
-        **Author: Kamil Pytlak ([GitHub](https://github.com/kamilpytlak/heart-condition-checker))**
-        
-        You can see the steps of building the model, evaluating it, and cleaning the data itself
-        on my GitHub repo [here](https://github.com/kamilpytlak/data-analyses/tree/main/heart-disease-prediction). 
-        """)
+
 
     heart = load_dataset()
 
@@ -144,13 +123,13 @@ def main():
                         f" heart disease is {round(prediction_prob[0][1] * 100, 2)}%."
                         f" You are healthy!**")
             st.image("images/heart-okay.jpg",
-                     caption="Your heart seems to be okay! - Dr. Logistic Regression")
+                     caption="Your heart seems to be okay! - Dr. Machine Learning")
         else:
             st.markdown(f"**The probability that you will have"
                         f" heart disease is {round(prediction_prob[0][1] * 100, 2)}%."
                         f" It sounds like you are not healthy.**")
             st.image("images/heart-bad.jpg",
-                     caption="I'm not satisfied with the condition of your heart! - Dr. Logistic Regression")
+                     caption="I'm not satisfied with the condition of your heart! - Dr. Machine Learning")
 
 
 if __name__ == "__main__":
